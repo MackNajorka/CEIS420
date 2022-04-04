25e715707798faddc700a5d63818a6debef3ff73
Mack N <Mnajorka@gmail.com>
2022-04-03 19:42:39 -0700

    Week 5 Lab, Restaurant Ordering System
    Recreate the above form with good design principles. The reset button should clear all items.
    The OK/Order button should take the user to another form that displays all the items chosen.

---
+++ Week5Lab/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week5Lab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddToOrderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                if (HamburgerBtn.IsChecked == true)
+                {
+                    listBox.Items.Add ("Hamburger");
+                }
+                else if (CheeseburgerBtn.IsChecked == true)
+                {
+                    listBox.Items.Add("Cheeseburger");
+
+                }
+
+                else if (ChickenBtn.IsChecked == true)
+                {
+                    listBox.Items.Add("Chicken Fingers");
+
+                }
+
+                else if (PizzaBtn.IsChecked == true)
+                {
+                    listBox.Items.Add("Pizza");
+
+                }
+
+                else if (MedFryBtn.IsChecked == true)
+                {
+                    listBox.Items.Add("Medium Fries");
+                }
+
+                else if (LargeFryBtn.IsChecked == true)
+                {
+                    listBox.Items.Add("Large Fries");
+                }
+            }
+        }
+
+        private void resetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+        }
+
+        private void Checkoutbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            listBox.Visibility = Visibility.Visible;
+            TotalLabel.Visibility = Visibility.Visible;
+            groupBox.Visibility = Visibility.Hidden;
+            resetBtn.Visibility = Visibility.Hidden;
+            AddToOrderBtn.Visibility = Visibility.Hidden;
+            CheckoutBtn.Visibility = Visibility.Hidden;
+        }
+    }
+}
